f63cdbc6b76fa893aed60a91aaf2c9e6b487ad93
amine <ahmane.m.amine@gmail.com>
2023-05-08 18:04:30 +0100

    feat: Solution by Optimizer ID
    Solution can be retrieved using linked Optimizer id

--- api/optimized_route/views.py
+++ api/optimized_route/views.py
@@ -1,11 +1,25 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .serializers import OptimizedRouteSerializer
 from .models import OptimizedRoute
 from rest_framework.decorators import action
 from rest_framework.response import Response
 import json
 
+from api.optimizer.models import Optimizer
+
 
 class OptimizedRouteViewSet(viewsets.ModelViewSet):
     queryset = OptimizedRoute.objects.all()
     serializer_class = OptimizedRouteSerializer
+
+    @action(detail=False, methods=['get'])
+    def solutionById(self, request):
+
+        pk = request.query_params.get('key')
+        try:
+            optimizer_instance = OptimizedRoute.objects.get(optimizer_id=pk)
+        except Optimizer.DoesNotExist:
+            return Response({'Error': f'Optimizer with id {pk} does not exist'}, status.HTTP_400_BAD_REQUEST)
+
+        instance = OptimizedRouteSerializer(optimizer_instance)
+        return Response(instance.data)
